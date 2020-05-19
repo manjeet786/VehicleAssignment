73d58eb7824e13e01f0956d753ebd6c505b795a2
Manjeet Choudhary <manjeetchoudhary193@gmail.com>
2020-05-19 18:18:00 +0530

    manjeet

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.css';
 import React from 'react';
 import Vehicle from'./ManageVehicles/Vehicle.js'
-import employee from'./ManageVehicles/Vehicle.js'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Vehicle />
-      <employee />
+      
     </div>
   );
 }
